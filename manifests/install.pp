635ee4569cec08a07a6a92c35f66ad4fd587f4dd
Michael McEnroe <michael.mcenroe@users.noreply.github.com>
2024-02-13 13:35:49 -0600

    disable module management of packages

--- manifests/install.pp
+++ manifests/install.pp
@@ -52,59 +52,59 @@ class graphite::install inherits graphite::params {
   #           python-django-tagging, python-simplejson
   # optional: python-ldap, python-memcache, memcached, python-sqlite
 
-  ensure_packages($::graphite::params::graphitepkgs)
-
-  create_resources('package', {
-    'carbon'         => {
-      ensure  => $::graphite::gr_carbon_ver,
-      name    => $::graphite::gr_carbon_pkg,
-      source  => $::graphite::gr_carbon_source,
-    }
-    ,
-    'django-tagging' => {
-      ensure => $::graphite::gr_django_tagging_ver,
-      name   => $::graphite::gr_django_tagging_pkg,
-      source => $::graphite::gr_django_tagging_source,
-    }
-    ,
-    'graphite-web'   => {
-      ensure => $::graphite::gr_graphite_ver,
-      name   => $::graphite::gr_graphite_pkg,
-      source => $::graphite::gr_graphite_source,
-    }
-    ,
-    'twisted'        => {
-      ensure => $::graphite::gr_twisted_ver,
-      name   => $::graphite::gr_twisted_pkg,
-      source => $::graphite::gr_twisted_source,
-      before => [Package['txamqp'], Package['carbon'],],
-    }
-    ,
-    'txamqp'         => {
-      ensure => $::graphite::gr_txamqp_ver,
-      name   => $::graphite::gr_txamqp_pkg,
-      source => $::graphite::gr_txamqp_source,
-      before => [
-        Package['carbon'],
-        ],
-    }
-    ,
-    'whisper'        => {
-      ensure => $::graphite::gr_whisper_ver,
-      name   => $::graphite::gr_whisper_pkg,
-      source => $::graphite::gr_whisper_source,
-    }
-    ,
-  }
-  , {
-    provider        => $gr_pkg_provider,
-    require         => $gr_pkg_require,
-    install_options => $gr_pkg_provider ? {
-      'pip'   => $pip_install_options,
-      default => undef,
-    },
-  }
-  )
+  #  ensure_packages($::graphite::params::graphitepkgs)
+  #
+  #  create_resources('package', {
+  #    'carbon'         => {
+  #      ensure  => $::graphite::gr_carbon_ver,
+  #      name    => $::graphite::gr_carbon_pkg,
+  #      source  => $::graphite::gr_carbon_source,
+  #    }
+  #    ,
+  #    'django-tagging' => {
+  #      ensure => $::graphite::gr_django_tagging_ver,
+  #      name   => $::graphite::gr_django_tagging_pkg,
+  #      source => $::graphite::gr_django_tagging_source,
+  #    }
+  #    ,
+  #    'graphite-web'   => {
+  #      ensure => $::graphite::gr_graphite_ver,
+  #      name   => $::graphite::gr_graphite_pkg,
+  #      source => $::graphite::gr_graphite_source,
+  #    }
+  #    ,
+  #    'twisted'        => {
+  #      ensure => $::graphite::gr_twisted_ver,
+  #      name   => $::graphite::gr_twisted_pkg,
+  #      source => $::graphite::gr_twisted_source,
+  #      before => [Package['txamqp'], Package['carbon'],],
+  #    }
+  #    ,
+  #    'txamqp'         => {
+  #      ensure => $::graphite::gr_txamqp_ver,
+  #      name   => $::graphite::gr_txamqp_pkg,
+  #      source => $::graphite::gr_txamqp_source,
+  #      before => [
+  #        Package['carbon'],
+  #        ],
+  #    }
+  #    ,
+  #    'whisper'        => {
+  #      ensure => $::graphite::gr_whisper_ver,
+  #      name   => $::graphite::gr_whisper_pkg,
+  #      source => $::graphite::gr_whisper_source,
+  #    }
+  #    ,
+  #  }
+  #  , {
+  #    provider        => $gr_pkg_provider,
+  #    require         => $gr_pkg_require,
+  #    install_options => $gr_pkg_provider ? {
+  #      'pip'   => $pip_install_options,
+  #      default => undef,
+  #    },
+  #  }
+  #  )
 
   if $::graphite::gr_django_pkg {
     $django_install_options = $::graphite::gr_django_provider ? {
